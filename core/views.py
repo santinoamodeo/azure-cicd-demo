b03784545f5502fac23e73d6282d9c7f0cf8490c
Santino Amodeo <santinoamodeo.dev@gmail.com>
2026-02-21 21:02:48 -0300

    Rollback to dad47a9

--- core/views.py
+++ core/views.py
@@ -3,5 +3,4 @@ from django.http import JsonResponse
 
 def health(request):
     env = os.environ.get("APP_ENV", "dev")
-    version = os.environ.get("APP_VERSION", "unknown")
-    return JsonResponse({"status": "ok", "env": env, "version": version})
+    return JsonResponse({"status": "ok", "env": env})
